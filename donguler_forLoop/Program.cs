40a7911d87d60889579023370c3f4e3fc33cdaf0
yucel taskiran <77216091+yucel21@users.noreply.github.com>
2021-08-11 04:21:14 +0300

    11 August

---
+++ donguler_forLoop/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace donguler_forLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Ekrandan girilen sayıya kadar olan tek sayılarını ekrana yazdır
+            Console.WriteLine("Bir sayı giriniz: ");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            //1 ile 1000 arasındaki tek ve çift sayıların kendi içlerinde toplamlarını ekrana yazdır.
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tekToplam += i;
+                }
+                else
+                {
+                    ciftToplam += i;
+                }
+            }
+
+            Console.WriteLine("Tek Toplam: " + tekToplam);
+            Console.WriteLine("Çift Toplam: " + ciftToplam);
+
+            //break, continue
+            Console.WriteLine("***Break***");
+            for (int j = 1; j < 10; j++)
+            {
+                if (j == 4)
+                    break;
+                Console.WriteLine(j);
+
+            }
+
+            Console.WriteLine("***Continue***");
+            for (int k = 1; k < 10; k++)
+            {
+                if (k == 4)
+                    continue;
+                Console.WriteLine(k);
+
+            }
+        }
+    }
+
+    /* Örnek:
+     *         #
+     *        ##
+     *       ###
+     *      ####
+     * Şeklinde bir görüntü elde etmek için aşağıdaki algoritma yazılmıştır
+     */
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Result.staircase(n);
+        }
+    }
+
+    class Result
+    {
+
+        /*
+         * Complete the 'staircase' function below.
+         *
+         * The function accepts INTEGER n as parameter.
+         */
+
+        public static void staircase(int n)
+        {
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(new String('#', i + 1).PadLeft(n, ' '));
+        }
+
+    }
+}
---
+++ if_elseif/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace if_elseif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time>=6 && time<12)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if(time >= 12 && time < 18)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler");
+            }
+
+            string sonuc = time <= 18 ? "İyi Günler!" : "İyi Geceler!";
+
+            sonuc = time >= 6 && time < 12 ? "Günaydın!" : time >= 12 && time < 18 ? "İyi Günler!!" : "İyi Geceler";
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
